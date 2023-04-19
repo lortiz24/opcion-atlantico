21ac30ece29817c52d9e47b1809542e70608d485
lortiz24 <ortizfrancoluisjavier@gmail.com>
2023-04-19 02:24:26 -0500

    responsive

--- src/features/eventModule/EventView.tsx
+++ src/features/eventModule/EventView.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, Col, List, Row, Space, Tabs, Tooltip, Typography } from 'antd'
+import { Avatar, Button, Card, Col, List, Row, Space, Tabs, Tooltip, Typography } from 'antd'
 import React, { useEffect, useState } from 'react'
 import { IEvents, IEventsRender } from './interfaces/events-interfaces'
 import useGetEvents from '../../hooks/useGetEvents';
@@ -6,6 +6,7 @@ import { DeleteOutlined, EditOutlined, LikeOutlined, MessageOutlined, StarOutlin
 import { LevelTitlesModules } from '../../settings/properties-globals/levels-titles';
 import GenerateQr from './components/GenerateQr';
 import ReadQr from './components/ReadQr';
+import useIsMobile from '../../hooks/useIsMobile';
 
 const IconText = ({ icon, text }: { icon: React.FC; text: string }) => (
     <Space>
@@ -21,7 +22,7 @@ const EventView = () => {
     const [openGenerateQR, setOpenGenerateQR] = useState(false)
     const [eventAttendanceId, setEventAttendanceId] = useState('')
     const [isOpenReadQR, setisOpenReadQR] = useState(false)
-
+    const { isMobile } = useIsMobile()
     const onGenerateQR = (eventId: string) => {
         setOpenGenerateQR(true)
         setEventAttendanceId(eventId)
@@ -45,6 +46,7 @@ const EventView = () => {
             <Typography.Title level={LevelTitlesModules}>Eventos</Typography.Title>
             <br />
             <List
+
                 loading={loading}
                 itemLayout="vertical"
                 size="large"
@@ -56,38 +58,54 @@ const EventView = () => {
                 }}
                 dataSource={events}
                 renderItem={(eventItem) => (
-                    <List.Item
-                        key={eventItem.title}
-                        actions={[
-                            <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-                            <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-                            <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
-                            <Button type='ghost' onClick={() => onGenerateQR(eventItem.id)}>Generar QR</Button>,
-                            <Button type='primary' onClick={() => onReadQr()}>Marcar Asistencia</Button>
-                        ]}
-                        extra={
-                            <img
-                                width={272}
-                                alt="logo"
-                                src={eventItem.img}
-                            />
-                        }
-                    >
-                        <List.Item.Meta
-                            avatar={<Avatar.Group maxCount={4} maxStyle={{ color: 'white', backgroundColor: '#333F44' }}>
-                                {eventItem.assistants.map((assistant, key) => (
-                                    <Tooltip key={key} title={assistant.name} placement='top'>
-                                        <Avatar style={{ backgroundColor: '#333F44', color: 'white' }}>
-                                            {assistant.name.charAt(0).toUpperCase()}
-                                        </Avatar>
-                                    </Tooltip>
-                                ))}
-                            </Avatar.Group>}
-                            title={eventItem.title}
-                            description={eventItem.date.toDate().toString()}
-                        />
-                        {eventItem.desciption}
-                    </List.Item>
+                    <>
+                        {isMobile ? (<List.Item
+                            key={eventItem.title}
+                            actions={[
+                                <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+                                <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+                                <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+                                <Button type='ghost' onClick={() => onGenerateQR(eventItem.id)}>Generar QR</Button>,
+                                <Button type='primary' onClick={() => onReadQr()}>Marcar Asistencia</Button>
+                            ]}
+
+                        >
+                            <Card title={eventItem.title}>Card content</Card>
+                        </List.Item>) : (
+                            <List.Item
+                                key={eventItem.title}
+                                actions={[
+                                    <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+                                    <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+                                    <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+                                    <Button type='ghost' onClick={() => onGenerateQR(eventItem.id)}>Generar QR</Button>,
+                                    <Button type='primary' onClick={() => onReadQr()}>Marcar Asistencia</Button>
+                                ]}
+                                extra={
+                                    <img
+                                        width={272}
+                                        alt="logo"
+                                        src={eventItem.img}
+                                    />
+                                }
+                            >
+                                <List.Item.Meta
+                                    avatar={<Avatar.Group maxCount={4} maxStyle={{ color: 'white', backgroundColor: '#333F44' }}>
+                                        {eventItem.assistants.map((assistant, key) => (
+                                            <Tooltip key={key} title={assistant.name} placement='top'>
+                                                <Avatar style={{ backgroundColor: '#333F44', color: 'white' }}>
+                                                    {assistant.name.charAt(0).toUpperCase()}
+                                                </Avatar>
+                                            </Tooltip>
+                                        ))}
+                                    </Avatar.Group>}
+                                    title={eventItem.title}
+                                    description={eventItem.date.toDate().toString()}
+                                />
+                                {eventItem.desciption}
+                            </List.Item>)}
+                    </>
+
                 )}
             />
 
