ed8fbc8b58c564ba62af71fdd4c294da90ea1937
lortiz24 <ortizfrancoluisjavier@gmail.com>
2023-04-25 12:42:04 -0500

    Datos del usuario

--- src/features/my-profile/MyProfileView.tsx
+++ src/features/my-profile/MyProfileView.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useAppSelector } from '../../store/store'
 
 const MyProfileView = () => {
-  return (
-    <div>MyProfileView</div>
-  )
+    const { displayName, photoURL, uid, email, userInfo } = useAppSelector(selector => selector.auth)
+    return (
+        <div>MyProfileView</div>
+    )
 }
 
 export default MyProfileView
