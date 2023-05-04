693bf68b6c71a678d82da240d5857560a3e641d5
lortiz24 <ortizfrancoluisjavier@gmail.com>
2023-05-04 12:31:55 -0500

    fix: justify de my profile

--- src/features/my-profile/MyProfileView.tsx
+++ src/features/my-profile/MyProfileView.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import FormProfileComponent from './components/FormProfileComponent';
-import { Avatar, Col, Row, Typography } from 'antd';
+import { Avatar, Col, Row, Space, Typography } from 'antd';
 import { IUserInfo, IUserInfoForm } from '../../interfaces/user-interfaces';
 import { useAppDispatch, useAppSelector } from '../../store/store';
 import { startUpdateUserProfile } from '../../store/auth';
@@ -22,18 +22,19 @@ const MyProfileView = () => {
 
 	return (
 		<Row justify='center' style={{ marginTop: '3%' }}>
-			<Col xs={22} md={12} lg={8}>
-				<Row justify='center'>
+			<Col xs={24} md={12} lg={8}>
+				<Space style={{ display: 'flex', justifyContent: 'center' }}>
 					<Title level={1}>Editar Perfil</Title>
-				</Row>
-				<Row justify='center' style={{ marginBottom: '3%' }}>
+				</Space>
+				<Space style={{ display: 'flex', justifyContent: 'center' }}>
 					<Avatar
 						size={150}
-						src={photoURL?photoURL:'https://firebasestorage.googleapis.com/v0/b/opcion-atlantico.appspot.com/o/avatar-defecto.webp?alt=media&token=dc44ebc7-da97-4d93-8a03-c8f62103054e'}
+						src={photoURL ? photoURL : 'https://firebasestorage.googleapis.com/v0/b/opcion-atlantico.appspot.com/o/avatar-defecto.webp?alt=media&token=dc44ebc7-da97-4d93-8a03-c8f62103054e'}
 					/>
-				</Row>
+				</Space>
+
 			</Col>
-			<Col xs={22} md={12} lg={8} style={{ marginRight: '50px' }}>
+			<Col xs={24} md={12} lg={8} >
 				<FormProfileComponent onSubmitForm={onSubmitForm} />
 			</Col>
 		</Row>
